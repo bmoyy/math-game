73fe9b7b304feccbf7607bbad193adb1df7beea0
Bastien Moy <moy.bastien@gmail.com>
2023-06-26 18:15:11 -0700

    adding end_game function

--- classes/Master.rb
+++ classes/Master.rb
@@ -38,16 +38,33 @@ class Master
     if !question.correct?(guess)
       @active_player.lose_life
       puts "That's Wrong!"
-      puts "P1: #{@player1.lives}/3 vs P2: #{@player2.lives}/3"
+      puts "#{@player1.name}: #{@player1.lives}/3 vs #{@player2.name}: #{@player2.lives}/3"
     else
       puts "That's Correct!"
       puts "P1: #{@player1.lives}/3 vs P2: #{@player2.lives}/3"
     end
 
+    if @player1.lives == 0 || @player2.lives == 0
+      self.end_game
+    else
+      self.run_round
+    end
   end
 
   def switch_player
     @active_player == @player1 ? @active_player = @player2 : @active_player = @player1
+  end
+
+  def end_game
+    puts ""
+    if @player1.lives == 0
+      puts "#{@player2.name} wins with a score of #{@player2.lives}/3!"
+    elsif @player2.lives == 0
+      puts "#{@player1.name} wins with a score of #{@player1.lives}/3!"
+    end
+
+    puts "----- GAME OVER -----"
+
   end
 
 end
